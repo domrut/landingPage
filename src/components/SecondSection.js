fdb75a631954f014b75a6b2421cf67e16bf001f5
domrut <d.rutkauskas@protonmail.com>
2023-07-03 19:12:26 +0300

    tabSection

---
+++ src/components/SecondSection.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {FirstSectionHeader, Heading2} from "./styled/FirstSection.styled";
+import {Text} from "../Texts";
+import {Paragraph} from "./styled/Paragraph.styled";
+import {Pill} from "./styled/SecondSection.styled";
+import Tab from "./styled/Tab";
+import {Divider} from "./styled/Divider.styled";
+
+function SecondSection() {
+    return (
+        <section>
+            <Pill><p>{Text.secondSectionHeaderPill}</p></Pill>
+            <FirstSectionHeader>
+                <Heading2 marginTop="1.25rem">{Text.secondSectionHeader}</Heading2>
+                <Paragraph maxWidth="740px">{Text.firstSectionParagraph}</Paragraph>
+            </FirstSectionHeader>
+            {Text.tabHeader.map((el, i) => {
+                return <Tab key={i} element={i}/>
+            })}
+            <Divider/>
+        </section>
+    );
+}
+
+export default SecondSection;
--- src/components/styled/Paragraph.styled.js
+++ src/components/styled/Paragraph.styled.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const Paragraph = styled.p`
   color: #9BA9B4;
-  text-align: center;
+  text-align: ${({textAlign}) => textAlign || "center"};
   font-size: ${({fontSize}) => fontSize || "1.25rem"};
   font-style: normal;
   max-width: ${({maxWidth}) => maxWidth || "670px"};
-  margin: 0;
+  margin: ${({margin}) => margin || 0};
   font-weight: 400;
   line-height: 1.875rem;
   letter-spacing: -0.01563rem;
